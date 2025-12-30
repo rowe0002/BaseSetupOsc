cfded5921296ab0286164437796644a02cff7113
rowe0002 <robert.rowe@nyu.edu>
2025-12-30 12:14:55 -0500

    moved files to Supporting Files folder

---
+++ BaseSetup/Supporting Files/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  BaseSetup
+//
+//  Created by Robert Rowe on 12/30/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
